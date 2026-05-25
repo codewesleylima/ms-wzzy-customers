a269f9dff5589c93508ea59745640b52958c86ce
codewesleylima <gusterlima@hotmail.com>
2026-05-24 23:44:21 -0300

    feat(ms-customers): initialize project structure
    - Add build configuration
    - Add Spring Boot application class
    - Add initial test skeleton

---
+++ src/main/java/com/wzzy/library/mscustomers/MsCustomersApplication.java
@@ -0,0 +1,13 @@
+package com.wzzy.library.mscustomers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCustomersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsCustomersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wzzy/library/mscustomers/MsCustomersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wzzy.library.mscustomers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCustomersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
