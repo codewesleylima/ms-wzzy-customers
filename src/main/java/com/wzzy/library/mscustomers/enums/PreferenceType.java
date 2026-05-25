ef96cd90ac9c991bff79ace114f5d273fa5eb1b3
codewesleylima <gusterlima@hotmail.com>
2026-05-24 23:44:41 -0300

    feat(ms-customers): add PreferenceType enum

---
+++ src/main/java/com/wzzy/library/mscustomers/enums/PreferenceType.java
@@ -0,0 +1,11 @@
+package com.wzzy.library.mscustomers.enums;
+
+public enum PreferenceType {
+    CATEGORY,
+    AUTHOR,
+    NOTIFICATION_EMAIL,
+    NOTIFICATION_SMS,
+    LANGUAGE,
+    CURRENCY,
+    THEME
+}
